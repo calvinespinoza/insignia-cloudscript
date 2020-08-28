957cdfb67eafce41104c96c96722a13ca4a01eb8
Calvin <calvinespinoza@Calvins-MacBook-Pro.local>
2020-08-28 00:27:39 -0600

    fix getCurrentDate function

--- UpdateAllPlayerStatistics.js
+++ UpdateAllPlayerStatistics.js
@@ -98,14 +98,30 @@ function addPointsHistory(value, entityProfile) {
     }
 }
 
-function getCurrentDate()
-{
-    var date = new Date();
-    var DD = date.getDate();
-    var MM = date.getMonth() + 1;
-    var YY = date.getFullYear().substring(2,4);
-    var hh = date.getHours();
-    var mm = date.getMinutes();
+function getCurrentDate() {
+    var now = new Date();
+    var year = now.getFullYear().toString().substring(2,4);
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
 
-    return `${DD}/${MM}/${YY} ${hh}:${mm}`
+    if (month.toString().length == 1) {
+        month = '0' + month;
+    }
+    if (day.toString().length == 1) {
+        day = '0' + day;
+    }
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
+    }
+    if (minute.toString().length == 1) {
+        minute = '0' + minute;
+    }
+    if (second.toString().length == 1) {
+        second = '0' + second;
+    }
+    var dateTime = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
+    return dateTime;
 }
