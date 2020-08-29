d8b7775b4465fde226e53a9417fe06cba0152c56
Calvin <calvinespinoza@Calvins-MacBook-Pro.local>
2020-08-29 09:35:42 -0600

    add userData

--- GetUserData.js
+++ GetUserData.js
@@ -1,4 +1,20 @@
 handlers.getUserData = function (args, context) {
-    var userData = server.GetUserAccountInfo({ PlayFabId: currentPlayerId });
-    return { userData: userData }
+    var user = server.GetUserAccountInfo({ PlayFabId: currentPlayerId });
+
+    var userDataRequest = {
+        PlayFabId: currentPlayerId,
+        Keys: ["Age", "Country", "School"]
+    }
+
+    var userData = server.GetUserData(userDataRequest);
+
+    var returnObject = {
+        playFabId: currentPlayerId,
+        username: user.UserInfo.Username,
+        displayName: user.UserInfo.TitleInfo.DisplayName,
+        age: userData.Data.Age.Value,
+        country: userData.Data.Country.Value,
+        school: userData.Data.School.Value
+    }
+    return returnObject
 }
