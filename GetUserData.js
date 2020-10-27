b9efd81c55fb1a8974d3f1aeeb3eaf8b2e2f50d0
Calvin Espinoza <calvin_espinoza@hotmail.com>
2020-10-26 20:05:40 -0600

    add level progress to getUserData

--- GetUserData.js
+++ GetUserData.js
@@ -1,5 +1,6 @@
 handlers.getUserData = function (args, context) {
     var user = server.GetUserAccountInfo({ PlayFabId: currentPlayerId });
+    var titlePlayerAccount = user.UserInfo.TitleInfo.TitlePlayerAccount;
 
     var userDataRequest = {
         PlayFabId: currentPlayerId,
@@ -7,11 +8,12 @@ handlers.getUserData = function (args, context) {
     }
 
     var userData = server.GetUserData(userDataRequest);
-
     var stats = server.GetPlayerStatistics({ PlayFabId: currentPlayerId });
-
-    var pointsHistory = getUserPointsHistory(user.UserInfo.TitleInfo.TitlePlayerAccount);
-
+    
+    var entityObjects = getPlayerEntityObjects(titlePlayerAccount);
+    var pointsHistory = getUserPointsHistory(entityObjects);
+    var levelProgress = getUserLevelProgress(entityObjects);
+    
     var returnObject = {
         PlayFabId: currentPlayerId,
         Username: user.UserInfo.Username,
@@ -20,12 +22,13 @@ handlers.getUserData = function (args, context) {
         Country: userData.Data.Country.Value,
         School: userData.Data.School.Value,
         Statistics: stats.Statistics, 
-        PointsHistory: pointsHistory
+        PointsHistory: pointsHistory,
+        LevelProgress: levelProgress
     }
     return returnObject;
 }
 
-function getUserPointsHistory(entityProfile) {
+function getPlayerEntityObjects(entityProfile) {
     try {
         var entityObjects = entity.GetObjects({
             Entity: entityProfile
@@ -34,8 +37,17 @@ function getUserPointsHistory(entityProfile) {
         log.error(ex);
     }
     log.debug(entityObjects);
+    return entityObjects;
+}
+
+function getUserPointsHistory(entityObjects) {
     if (entityObjects.Objects.PointsHistory)
         return entityObjects.Objects.PointsHistory.DataObject.History;
+    return null
+}
 
+function getUserLevelProgress(entityObjects) {
+    if (entityObjects.Objects.LevelProgress)
+        return entityObjects.Objects.PointsHistory.DataObject;
     return null
 }
